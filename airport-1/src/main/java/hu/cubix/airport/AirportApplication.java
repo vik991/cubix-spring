bf3d1e720d09c8b745373fc04e336896a29bdd7a
Viktor <sjviktor191@gmail.com>
2024-11-29 11:02:45 +0100

    test modification

--- airport-1/src/main/java/hu/cubix/airport/AirportApplication.java
+++ airport-1/src/main/java/hu/cubix/airport/AirportApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AirportApplication implements CommandLineRunner {
+	//Test modification
 
 	@Autowired
 	DiscountService discountService;
